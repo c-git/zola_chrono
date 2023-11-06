bb3a7b2f9627e9329226cf48db3e178d68a339ae
One <43485962+c-git@users.noreply.github.com>
2023-11-05 23:26:41 -0500

    Add logging and clap setup

---
+++ src/cli.rs
@@ -0,0 +1,50 @@
+use clap::{Parser, ValueEnum};
+use log::LevelFilter;
+
+#[derive(Parser, Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Default)]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "Updates the `date` and `updated` fields of the pages front matter"
+)]
+pub struct Cli {
+    #[arg(
+        value_name = "PATH",
+        help = "The root folder to start at",
+        long_help = "The root folder to start at (usually content or the folder). Required to be in a repository with a clean working tree.",
+        default_value = "."
+    )]
+    pub root_path: String,
+
+    /// Set logging level to use
+    #[arg(long, short, value_enum, default_value_t = LogLevel::Info)]
+    pub log_level: LogLevel,
+}
+
+/// Exists to provide better help messages variants copied from LevelFilter as
+/// that's the type that is actually needed
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug, Default)]
+pub enum LogLevel {
+    /// Nothing emitted in this mode
+    #[default]
+    Off,
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+impl From<LogLevel> for LevelFilter {
+    fn from(value: LogLevel) -> Self {
+        match value {
+            LogLevel::Off => LevelFilter::Off,
+            LogLevel::Error => LevelFilter::Error,
+            LogLevel::Warn => LevelFilter::Warn,
+            LogLevel::Info => LevelFilter::Info,
+            LogLevel::Debug => LevelFilter::Debug,
+            LogLevel::Trace => LevelFilter::Trace,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use cli::Cli;
+use log::{info, LevelFilter};
+use log4rs::{
+    append::console::ConsoleAppender,
+    config::{Appender, Root},
+    encode::pattern::PatternEncoder,
+};
+mod cli;
+
+fn main() -> anyhow::Result<()> {
+    let cli: Cli = Cli::parse();
+    init_logging(cli.log_level.into())?;
+    info!("Cli: {cli:#?}");
+    run(&cli)
+}
+
+fn init_logging(log_level: LevelFilter) -> anyhow::Result<()> {
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{h({d} {l} {t} - {m})}{n}")))
+        .build();
+    let config = log4rs::Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(Root::builder().appender("stdout").build(log_level))?;
+    log4rs::init_config(config)?;
+    Ok(())
+}
+
+fn run(cli: &Cli) -> anyhow::Result<()> {
+    todo!()
 }
