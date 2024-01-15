dcdbb6788579d47ca967f3d505b403e280ec9081
One <43485962+c-git@users.noreply.github.com>
2024-01-14 23:22:47 -0500

    WIP updating tests

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,5 +1,3 @@
-use anyhow::bail;
-use cargo_util::paths;
 use std::fmt::Debug;
 use std::fs;
 use std::fs::File;
@@ -7,61 +5,25 @@ use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 use strum::EnumIter;
-use version_control_clean_check::check_version_control;
-use version_control_clean_check::CheckOptions;
-use version_control_clean_check::VCSError;
-use version_control_clean_check::VCSResult;
+use zola_chrono::run;
+use zola_chrono::Cli;
 
 mod git_commands;
 
-pub(crate) fn test_check_version_control(
-    opts: CheckOptions,
-    test_dir: TestDir,
-    expected: VCSResult<()>,
-) {
+pub enum ResultExpected {
+    IsOk,
+    IsErr,
+}
+
+pub(crate) fn test_run(mut cli: Cli, test_dir: TestDir, expected: ResultExpected) {
     let path = test_dir.to_canonicalized_path();
-    println!("Opts: {opts:#?}\ntest_dir: {test_dir:?}\nPath: {path:?}");
-    let actual = check_version_control(path, &opts);
-    match_results(actual, expected);
-}
-
-#[derive(Debug)]
-pub(crate) struct TestError(VCSError);
-
-impl From<VCSError> for TestError {
-    fn from(value: VCSError) -> Self {
-        Self(value)
-    }
-}
-
-impl<T: Debug> TryFrom<VCSResult<T>> for TestError {
-    type Error = anyhow::Error;
-
-    fn try_from(value: VCSResult<T>) -> Result<Self, Self::Error> {
-        if value.is_ok() {
-            bail!("Value is not error. Found: {value:?}");
-        }
-        Ok(Self(value.unwrap_err()))
-    }
-}
-
-impl PartialEq for TestError {
-    fn eq(&self, other: &Self) -> bool {
-        match (&self.0, &other.0) {
-            (VCSError::NoVCS, VCSError::NoVCS) => true,
-            (
-                VCSError::NotAllowedFilesFound {
-                    dirty_files: l_dirty_files,
-                    staged_files: l_staged_files,
-                },
-                VCSError::NotAllowedFilesFound {
-                    dirty_files: r_dirty_files,
-                    staged_files: r_staged_files,
-                },
-            ) => l_dirty_files == r_dirty_files && l_staged_files == r_staged_files,
-            (VCSError::GitError(..), _) | (VCSError::Anyhow(..), _) => false, // Never equal if not one of our local errors during testing
-            _ => core::mem::discriminant(&self.0) == core::mem::discriminant(&other.0),
-        }
+    cli.root_path = path.to_string_lossy().to_string();
+    println!("Cli: {cli:#?}\ntest_dir: {test_dir:?}\nPath: {path:?}");
+    let actual = run(&cli);
+    println!("run result: {actual:?}");
+    match expected {
+        ResultExpected::IsOk => assert!(actual.is_ok()),
+        ResultExpected::IsErr => assert!(actual.is_err()),
     }
 }
 
@@ -75,7 +37,7 @@ pub enum TestDir {
 }
 
 impl TestDir {
-    pub(crate) const TEST_DIR_BASE: &str = "tests/test_folders/";
+    pub(crate) const TEST_DIR_BASE: &'static str = "tests/test_folders/";
     pub(crate) fn to_path(&self) -> PathBuf {
         let base_test_folder = PathBuf::from(Self::TEST_DIR_BASE);
         let sub_folder = match self {
@@ -95,22 +57,6 @@ impl TestDir {
     }
 }
 
-pub(crate) fn match_results(actual: VCSResult<()>, expected: VCSResult<()>) {
-    match (&actual, &expected) {
-        (Ok(_), Ok(_)) => (),
-        (Ok(_), Err(_)) | (Err(_), Ok(_)) => {
-            panic!("Actual and Expected do not match.\nactual: {actual:?}\nexpected: {expected:?}")
-        }
-        (Err(..), Err(..)) => {
-            let actual_error = actual.unwrap_err();
-            let expected_error = expected.unwrap_err();
-            println!("---\nActual Error:\n{actual_error}\n");
-            println!("---\nExpected Error:\n{expected_error}\n---");
-            assert_eq!(TestError(actual_error), TestError(expected_error))
-        }
-    }
-}
-
 pub fn create_test_folder(test_dir: &TestDir) -> anyhow::Result<()> {
     // Skip if folder if it already exists (doesn't check that it is in the correct state)
     let path = test_dir.to_path();
@@ -120,7 +66,7 @@ pub fn create_test_folder(test_dir: &TestDir) -> anyhow::Result<()> {
 
     match test_dir {
         TestDir::NoVCS => {
-            paths::create_dir_all(&path)?;
+            cargo_util::paths::create_dir_all(&path)?;
             create_abc(&path)?;
         }
         TestDir::Clean => {
--- tests/version_control.rs
+++ tests/version_control.rs
@@ -2,25 +2,25 @@ use anyhow::Context;
 use rstest::{fixture, rstest};
 use std::path::PathBuf;
 use strum::IntoEnumIterator;
-use utils::{create_test_folder, TestDir as TD};
-use version_control_clean_check::{check_version_control, CheckOptions, VCSError, VCSResult};
+use utils::{
+    create_test_folder,
+    ResultExpected::{self, IsErr, IsOk},
+    TestDir as TD,
+};
+use zola_chrono::{run, Cli};
 
 mod utils;
 
 #[test]
 fn non_existent_folder() {
-    let mut opts = CheckOptions::new();
-    let non_existent_path = PathBuf::from("non_existent_path_bfEHgMV62y5S7LYn");
-    assert!(!non_existent_path.exists());
+    let mut cli = Cli::default();
+    let non_existent_path = "non_existent_path_bfEHgMV62y5S7LYn";
+    assert!(!PathBuf::from(non_existent_path).exists());
+    cli.root_path = non_existent_path.to_string();
 
-    // Test is no vcs
-    let actual = check_version_control(&non_existent_path, &opts);
-    utils::match_results(actual, Err(VCSError::NoVCS));
-
-    // Test passes if no vcs allowed
-    opts.allow_no_vcs = true;
-    let actual = check_version_control(&non_existent_path, &opts);
-    utils::match_results(actual, Ok(()));
+    // Ensure run fails if folder doesn't exist
+    let actual = run(&cli);
+    assert!(actual.is_err());
 }
 
 #[fixture]
@@ -41,49 +41,40 @@ fn create_dirs() -> anyhow::Result<()> {
 #[case(TD::StagedAndDirty)]
 fn allow_no_vcs(#[case] test_dir: utils::TestDir, create_dirs: &anyhow::Result<()>) {
     assert!(create_dirs.is_ok(), "{create_dirs:?}");
-    let mut opts = CheckOptions::new();
-    opts.allow_no_vcs = true;
-    let expected = Ok(());
-    utils::test_check_version_control(opts, test_dir, expected);
+    let expected = IsOk;
+    utils::test_run(
+        Cli {
+            unattended: true,
+            ..Default::default()
+        },
+        test_dir,
+        expected,
+    );
 }
 
 #[rstest]
-// No Dirty, No Staged
-#[case(false, false, TD::NoVCS, Err(VCSError::NoVCS))]
-#[case(false, false, TD::Clean, Ok(()))]
-#[case(false, false, TD::StagedOnly, Err(VCSError::NotAllowedFilesFound { dirty_files: vec![], staged_files: vec!["b".to_string(), "c".to_string()] }))]
-#[case(false, false, TD::DirtyOnly, Err(VCSError::NotAllowedFilesFound { dirty_files: vec!["b".to_string(), "c".to_string()], staged_files: vec![] }))]
-#[case(false, false, TD::StagedAndDirty, Err(VCSError::NotAllowedFilesFound { dirty_files: vec!["c".to_string()], staged_files: vec!["b".to_string()] }))]
-// No Dirty, Yes Staged
-#[case(false, true, TD::NoVCS, Err(VCSError::NoVCS))]
-#[case(false, true, TD::Clean, Ok(()))]
-#[case(false, true, TD::StagedOnly, Ok(()))]
-#[case(false, true, TD::DirtyOnly, Err(VCSError::NotAllowedFilesFound { dirty_files: vec!["b".to_string(), "c".to_string()], staged_files: vec![] }))]
-#[case(false, true, TD::StagedAndDirty, Err(VCSError::NotAllowedFilesFound { dirty_files: vec!["c".to_string()], staged_files: vec![] }))]
-// Yes Dirty, No Staged
-#[case(true, false, TD::NoVCS, Err(VCSError::NoVCS))]
-#[case(true, false, TD::Clean, Ok(()))]
-#[case(true, false, TD::StagedOnly, Err(VCSError::NotAllowedFilesFound { dirty_files: vec![], staged_files: vec!["b".to_string(), "c".to_string()] }))]
-#[case(true, false, TD::DirtyOnly, Ok(()))]
-#[case(true, false, TD::StagedAndDirty, Err(VCSError::NotAllowedFilesFound { dirty_files: vec![], staged_files: vec!["b".to_string()] }))]
-// Yes Dirty, Yes Staged
-#[case(true, true, TD::NoVCS, Err(VCSError::NoVCS))]
-#[case(true, true, TD::Clean, Ok(()))]
-#[case(true, true, TD::StagedOnly, Ok(()))]
-#[case(true, true, TD::DirtyOnly, Ok(()))]
-#[case(true, true, TD::StagedAndDirty, Ok(()))]
-fn vcs_required(
+//
+#[case(false, false, false, TD::NoVCS, IsErr)]
+#[case(false, false, false, TD::Clean, IsErr)]
+#[case(false, false, false, TD::StagedOnly, IsErr)]
+#[case(false, false, false, TD::DirtyOnly, IsErr)]
+#[case(false, false, false, TD::StagedAndDirty, IsErr)]
+fn test_with_unattended(
+    #[case] should_check_only: bool,
     #[case] allow_dirty: bool,
-    #[case] allow_staged: bool,
+    #[case] allow_no_vcs: bool,
     #[case] test_dir: utils::TestDir,
-    #[case] expected: VCSResult<()>,
+    #[case] expected: ResultExpected,
     create_dirs: &anyhow::Result<()>,
 ) {
     assert!(create_dirs.is_ok(), "{create_dirs:?}");
-    let mut opts = CheckOptions::new();
-    // opts.allow_no_vcs = false; // Always false because it is tested in allow_no_vcs
-    opts.allow_dirty = allow_dirty;
-    opts.allow_staged = allow_staged;
-
-    utils::test_check_version_control(opts, test_dir, expected);
+    let cli = Cli {
+        root_path: Default::default(),
+        unattended: true,
+        should_check_only,
+        allow_dirty,
+        allow_no_vcs,
+        log_level: Default::default(),
+    };
+    utils::test_run(cli, test_dir, expected);
 }
